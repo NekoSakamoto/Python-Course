42a5454871ce417e72202dd033defe3186af365a
NekoSakamoto <121953154+NekoSakamoto@users.noreply.github.com>
2023-02-03 18:08:17 +0500

    Add files via upload

---
+++ bakapro_1.py
@@ -0,0 +1,26 @@
+# the hash makes this line a single line coment, you can write anything because python will ignore what will be under this line
+''' this is a multi line comment it does the same thing like single line comment
+but it can comment multiple lines like this '''
+# print("Print fuction is used to print something") 
+# from here its practice
+''' print("""Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle, twinkle little star
+How I wonder what you are
+When the blazing sun is gone
+When he nothing shines upon
+Then you show your little light
+Twinkle, twinkle, all the night
+Twinkle, twinkle, little star
+How I wonder what you are""") '''# """ three quotes like this can print multiple lines (amazing not kidding)
+# lets import a module and run it 
+# from playsound import playsound
+# lets run/use it!
+# playsound ('/home/nekosamadesu/Downloads/Python Course with Notes/1. Chapter 1/play.mp3') 
+# i have commented this command because the music is too annoying and it doesnt stop like for the whole 4 minutes... ihate the song but it works!
+'''import os 
+print(os.listdir())''' # these two commands are just simply used to show the files of a directory where you currently are
+
+
