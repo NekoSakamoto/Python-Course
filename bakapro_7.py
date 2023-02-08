6b6c678cf9ae08b58d76b4e06334864f2e15fbce
NekoSakamoto <121953154+NekoSakamoto@users.noreply.github.com>
2023-02-08 20:03:11 +0500

    Add files via upload

---
+++ bakapro_7.py
@@ -0,0 +1,93 @@
+# todays topic are loops in python
+## WHILE LOOP
+# n1 = int(input("Enter a number: "))
+# while n1<=100:
+#     print ("Never gonna give you up",str(n1))
+#     n1 = n1 + 0.5
+
+# i = 0
+# while i<=5:
+#     print(i)
+#     i = i + 1
+
+# friends = ['Sufyan', 'Qureshi', 'Tayyab']
+# i = 0
+# while i<len(friends):
+#     print(friends[i])
+#     i = i + 1
+
+# l = [1,5,8]
+# for item in l:
+#     print(item)
+
+# for i in range(1, 8, 2):
+#     print(i)
+
+# for i in range(11):
+#     print(i)
+#     if i == 5:
+#         continue
+# else:
+#     print ("Else of for loop.")
+
+# i = 4
+# if i>0:
+#     pass 
+# while i>9:
+#     pass
+# print("Hello world")
+
+# n1 = int(input("Type the number to print:\n"))
+# n100 = int(input("Where to print:\n"))
+# for table in range(1, 11):
+    # print(f"{n1} x {table} = {n1*table}")
+#     print(n1,"x",table,"=",n1*table)
+
+# l1 = ["Harry", "Sohan", "Sachan", "Rohan"]
+# for names in l1:
+#     if names.startswith("S"):
+#         print("Good morning", names)
+
+# n1 = int(input("Type the number to to find out if the number is prime or not:\n"))
+# prime = True
+# for i in range(2, n1):
+#     if (n1%i == 0):
+#         prime = False
+#         break
+# if prime:
+#     print ("This number is prime.")
+# else:
+#     print ("This number is not prime.")
+
+# num = int(input("Enter your number: "))
+# factorial = 1
+# for x in range(1, num+1):
+#     factorial = factorial * x
+# print(f"The factorial of {num} is {factorial}.")
+
+# num1 = int(input("Enter your number: "))
+# num2 = int(input("Enter your number: "))
+# sum = 1
+# while sum:
+#     sum==num1 + num2
+# print(f"The sum of {num1} and {num2} is {sum}.") failed code
+
+# for i in range(9):
+#     print("*" * i)
+
+# n= 3
+# for i in range(3):
+#     print(" " * (n-i-1), end="")
+#     print("*" * (2*i+1), end="")
+#     print(" " * (n-i-1))
+
+# for i in range(3):
+#     print("***")
+#     print("* *")
+#     print("***") failed code
+
+# n1 = int(input("Type the number to print:\n"))
+# # n100 = int(input("Where to print:\n"))
+# for table in range(10, 0, -1):
+#     print(f"{n1} x {table} = {n1*table}")
+#     # print(n1,"x",table,"=",n1*table)
